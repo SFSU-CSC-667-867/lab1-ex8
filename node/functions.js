8a53fee0fd542e24febfe6c8f3eb3dc39a7575fd
Matt M <matt@puroxy.org>
2019-02-06 20:28:21 -0800

    functions

--- node/functions.js
+++ node/functions.js
@@ -6,14 +6,24 @@ function hi() {
 hi();
 
 // arrow functions
-const arrow = (test) => {
+const arrow = test => {
   console.log(test);
-}
+};
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
+const myArrow = y => y + 1;
+console.log(myArrow(5));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log(`tight`);
+
+functionFactory()();
+
+const a = functionFactory();
+a();
+const another = functionFactory;
+another()();
